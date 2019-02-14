c49e0cd617dbda40e2673078c627767985b6c855
Ergi SALA <ergisala@live.com>
2019-02-14 23:30:12 +0100

    TP2 Point 6 fait

--- indexInverse.py
+++ indexInverse.py
@@ -3,7 +3,7 @@ import os
 
 inputDirectory = "../json/"
 
-def indexInvers(indir):
+def indexInvers(indir, outdir):
     # chargement du vocabulaire
     vocabfh = open(indir+"vocabulaire.json", "r")
     vocab = json.load(vocabfh)
@@ -12,8 +12,18 @@ def indexInvers(indir):
     vectfh = open(indir+"vect.json", "r")
     vect = json.load(vectfh)
     vectfh.close()
+    # Le dictionnaire resultat
+    res = {}
+    for terme in vocab:
+        # dictionnaire idDoc: idf du terme dans le doc
+        tfidf = {}
+        for idDoc in vect:
+            if terme in vect[idDoc]:
+                tfidf[idDoc] = vect[idDoc][terme]
+        res[terme] = tfidf
+    # ecriture du resultat
+    f = open(outdir+"indexInverse.json", "w+")
+    json.dump(res, f)
+    f.close()
 
-
-    
-
-indexInvers(inputDirectory)
+indexInvers(inputDirectory, inputDirectory)
