1eb072ffb157cecb14e0e3ab427b1ff8eafa156c
Ergi SALA <ergisala@live.com>
2019-02-14 17:05:41 +0100

    TP2 Point 5 fait

---
+++ vect.py
@@ -0,0 +1,41 @@
+import os
+from math import log
+import json
+
+inputDirectory = "../antiDict/"
+
+def SaltonVect(indir):
+    # un dictionnaire qui stocke l'ensemble de tous les vecteurs de documents
+    # avec l'identifiant de document comme cle
+    res = {}
+    # chargement du corpus des documents
+    docs = os.listdir(indir)
+    N = len(docs)
+    # chargement du vocabulaire
+    vf = open("../vocabulaire.json", "r")
+    vocab = json.load(vf)
+    vf.close()
+    # Pour tout fichier sttr
+    for file in docs:
+        # le vecteur du document courant
+        dvect = {}
+        # words contient les mots du fichier sttr
+        fh = open(indir+file, "r")
+        words = fh.read().split(" ")
+        fh.close()
+        words.remove("") # Supprimer le mot vide car aucun interet
+        for i in words:
+            # calcul du idf_i
+            idf_i = log(N/vocab[i])
+            # Si le mot a ete vu dans le doc, ajouter idf_i
+            if i in dvect:
+                dvect[i] = dvect[i] + idf_i
+            # Si c'est la premiere occurrence du mot dans le doc, assigner idf_i
+            else:
+                dvect[i] = idf_i
+        # ajout du vecteur du doc dans res avec l'identifiant de document comme cle
+        res[int(file.strip("CACM-").strip(".sttr"))] = dvect
+    # verification du resultat
+    print(res)
+
+SaltonVect(inputDirectory)
--- vocabjson.py
+++ vocabjson.py
@@ -4,18 +4,21 @@ import json
 inputDirectory = "../antiDict/"
 
 def vocabjson(indir):
-    vj = {}
+    vj = {} # dictionnaire du vocabulaire.json
     for file in os.listdir(indir):
-        vjfh = open(indir+file, "r") 
+        vjfh = open(indir+file, "r") # vj fileHandler
         # Un set ne contient pas de duplicats. a voir comme list.uniq()
         words = set(vjfh.read().split(" "))
         vjfh.close()
+        # words ne contient pas de duplicats. Chaque i dans word est unique au sein du document.
+        # Donc la valeur vj[i] correspond bien au nombre du documents ou i apparait
         for i in words:
             if i in vj:
                 vj[i] = vj[i] + 1
             else:
-                vj[i] = 1
-    vj.pop("")
+                vj[i] = 1    
+    vj.pop("") # Supprimer le mot vide car aucun interet
+    # Ecriture dans le fichier json
     f = open("../vocabulaire.json", "w+")
     json.dump(vj, f)
     f.close()
