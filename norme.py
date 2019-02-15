ce7283d4b3617b4814577fa7db01d278be7cfd5b
Ergi SALA <ergisala@live.com>
2019-02-15 12:26:07 +0100

    TP2 Point 7fait

---
+++ norme.py
@@ -0,0 +1,25 @@
+import json
+import os
+from math import sqrt
+
+inputDirectory = "../json/"
+
+def norme (indir, outdir):
+    # chargement des vecteurs de docs
+    vectfh = open(indir+"vect.json", "r")
+    vect = json.load(vectfh)
+    vectfh.close()
+    # dictionnaire resultat
+    res = {}
+    # calcul de la norme
+    for idDoc in vect:
+        norm = 0
+        for terme in vect[idDoc]:
+            norm = norm + (vect[idDoc][terme]*vect[idDoc][terme])
+        res[idDoc] = sqrt(norm)
+    # verification du resultat
+    f = open(outdir+"norm.json", "w+")
+    json.dump(res, f)
+    f.close()
+
+norme (inputDirectory, inputDirectory)
